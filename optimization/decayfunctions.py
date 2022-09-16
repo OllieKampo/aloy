ea0ee8f5525ca20626fb234e797759236f7a4cd2
Oliver Kamperis <olliekampo@gmail.com>
2022-09-16 13:00:28 +0100

    Update decay functions.

--- optimization/decayfunctions.py
+++ optimization/decayfunctions.py
@@ -43,7 +43,7 @@ def linear_decay_function(initial_value: float,
     """
     Create a linear parameter decay function.
     
-    The parameter value reduces linearly from the initial value to the final value over the decay range.
+    The parameter value changes linearly from the initial value to the final value over the decay range.
     Where the decay range is the range of steps between the decay start step (inclusive) and decay stop step (exclusive).
     
     Parameters
@@ -51,21 +51,31 @@ def linear_decay_function(initial_value: float,
     `initial_value : float` - The initial value of the parameter.
     
     `final_value : float` - The final value of the parameter.
+    If the final value is less than the initial value, the parameter value will decrease over the decay range.
+    If the final value is greater than the initial value, the value will increase.
     
     `decay_start : int` - The inclusive start step of the decay range.
     
     `decay_stop : int` - The exclusive stop step of the decay range.
     
-    `decay_rate : float` - The decay rate, this is the factor of the decay range to which the parameter approaches its final value.
-    For example, a decay constant of 0.5 will result in a line that approaches the final value half way between the decay start and decay end bound.
+    `decay_rate : float = 1.0` - The decay rate, this is the factor of the decay range to which the parameter approaches its final value.
+    For example, a decay rate of 0.5 will result in a line that approaches the final value half way between the decay start and decay end bound.
     
     Returns
     -------
     `(int) -> float` - A linear parameter decay function, returns the parameter value at a given iteration.
     The arguments given to this functions are assigned as attributes to the function instance returned.
+    
+    Raises
+    ------
+    `ValueError` - If the decay rate is less than 0.0.
+    
+    `ValueError` - If the decay start is greater than the decay end.
     """
     if decay_rate < 0.0:
-        raise ValueError(f"Decay constant must be greater than 0.0. Got; {decay_rate}.")
+        raise ValueError(f"Decay rate must be greater than 0.0. Got; {decay_rate}.")
+    if decay_start > decay_end:
+        raise ValueError(f"Decay start must be less than decay end. Got; {decay_start} > {decay_end}.")
     
     ## Decay is exclusive of the decay end iteration.
     decay_end -= 1
@@ -80,7 +90,7 @@ def linear_decay_function(initial_value: float,
         decay_iterations: int = min(decay_range, max(0, iteration - decay_start))
         if decay_iterations == 0:
             return initial_value
-        elif decay_iterations < decay_range_rated: ## TODO: Support decay and growth.
+        elif decay_iterations < decay_range_rated:
             return initial_value - (value_range * (decay_iterations / decay_range_rated))
         return final_value
     
@@ -100,9 +110,40 @@ def polynomial_decay_function(initial_value: float,
                               ) -> Callable[[int], float]:
     """
     Create a polynomial decay function.
+    
+    The parameter value changes polynomially from the initial value to the final value over the decay range.
+    Where the decay range is the range of steps between the decay start step (inclusive) and decay stop step (exclusive).
+    
+    Parameters
+    ----------
+    `initial_value : float` - The initial value of the parameter.
+    
+    `final_value : float` - The final value of the parameter.
+    If the final value is less than the initial value, the parameter value will decrease over the decay range.
+    If the final value is greater than the initial value, the value will increase.
+    
+    `decay_start : int` - The inclusive start step of the decay range.
+    
+    `decay_stop : int` - The exclusive stop step of the decay range.
+    
+    `decay_rate : float = 1.0` - The decay rate, this is the factor of the decay range to which the parameter approaches its final value.
+    For example, a decay rate of 0.5 will result in a line that approaches the final value half way between the decay start and decay end bound.
+    
+    Returns
+    -------
+    `(int) -> float` - A polynomial parameter decay function, returns the parameter value at a given iteration.
+    The arguments given to this functions are assigned as attributes to the function instance returned.
+    
+    Raises
+    ------
+    `ValueError` - If the decay rate is less than 0.0.
+    
+    `ValueError` - If the decay start is greater than the decay end.
     """
     if decay_rate < 0.0:
-        raise ValueError(f"Decay constant must be greater than 0.0. Got; {decay_rate}.")
+        raise ValueError(f"Decay rate must be greater than 0.0. Got; {decay_rate}.")
+    if decay_start > decay_end:
+        raise ValueError(f"Decay start must be less than decay end. Got; {decay_start} > {decay_end}.")
     
     ## Decay is exclusive of the decay end iteration.
     decay_end -= 1
@@ -130,26 +171,6 @@ def polynomial_decay_function(initial_value: float,
     
     return polynomial_decay
 
-# def exponential_decay_function(initial_value: float,
-#                                decay_constant: float,
-#                                decay_start: int,
-#                                decay_end: int,
-#                                iterations_limit: int
-#                                ) -> Callable[[int], float]:
-#     """
-#     Create an exponential decay function.
-#     """
-#     if not (0.0 <= decay_constant <= 1.0):
-#         raise ValueError(f"Decay constant must be between 0.0 and 1.0. Got; {decay_constant}.")
-    
-#     decay_limit: int = min(iterations_limit, decay_end) - 1
-    
-#     def exponential_decay(iteration: int) -> float:
-#         decay_iterations: int = min(decay_limit - decay_start, max(0, iteration - decay_start))
-#         return initial_value * math.exp(-((1.0 - decay_constant) * decay_iterations))
-    
-#     return exponential_decay
-
 def exponential_decay_function(initial_value: float,
                                final_value: float,
                                decay_start: int,
@@ -157,10 +178,41 @@ def exponential_decay_function(initial_value: float,
                                decay_rate: float = 1.0
                                ) -> Callable[[int], float]:
     """
-    Create a bounded exponential decay function.
+    Create an exponential decay function.
+    
+    The parameter value changes exponentially from the initial value to the final value over the decay range.
+    Where the decay range is the range of steps between the decay start step (inclusive) and decay stop step (exclusive).
+    
+    Parameters
+    ----------
+    `initial_value : float` - The initial value of the parameter.
+    
+    `final_value : float` - The final value of the parameter.
+    If the final value is less than the initial value, the parameter value will decrease over the decay range.
+    If the final value is greater than the initial value, the value will increase.
+    
+    `decay_start : int` - The inclusive start step of the decay range.
+    
+    `decay_stop : int` - The exclusive stop step of the decay range.
+    
+    `decay_rate : float = 1.0` - The decay rate, this is the factor of the decay range to which the parameter approaches its final value.
+    For example, a decay rate of 0.5 will result in a line that approaches the final value half way between the decay start and decay end bound.
+    
+    Returns
+    -------
+    `(int) -> float` - A exponential parameter decay function, returns the parameter value at a given iteration.
+    The arguments given to this functions are assigned as attributes to the function instance returned.
+    
+    Raises
+    ------
+    `ValueError` - If the decay rate is less than 0.0.
+    
+    `ValueError` - If the decay start is greater than the decay end.
     """
     if decay_rate < 0.0:
-        raise ValueError(f"Decay constant must be greater than 0.0. Got; {decay_rate}.")
+        raise ValueError(f"Decay rate must be greater than 0.0. Got; {decay_rate}.")
+    if decay_start > decay_end:
+        raise ValueError(f"Decay start must be less than decay end. Got; {decay_start} > {decay_end}.")
     
     ## Decay is exclusive of the decay end iteration.
     decay_end -= 1
@@ -179,6 +231,12 @@ def exponential_decay_function(initial_value: float,
             return initial_value - (value_range * (math.log(decay_iterations) / math.log(decay_range_rated)))
         return final_value
     
+    ## Assign arguments as attributes to the function.
+    _locals = locals()
+    for param in linear_decay_function.__annotations__:
+        if param != "return":
+            setattr(exponential_decay, param, _locals[param])
+    
     return exponential_decay
 
 def sinusoidal_decay_function(initial_value: float,
@@ -189,9 +247,40 @@ def sinusoidal_decay_function(initial_value: float,
                               ) -> Callable[[int], float]:
     """
     Create a sinusoidal decay function.
+    
+    The parameter value changes along a sinusoidal curve from the initial value to the final value over the decay range.
+    Where the decay range is the range of steps between the decay start step (inclusive) and decay stop step (exclusive).
+    
+    Parameters
+    ----------
+    `initial_value : float` - The initial value of the parameter.
+    
+    `final_value : float` - The final value of the parameter.
+    If the final value is less than the initial value, the parameter value will decrease over the decay range.
+    If the final value is greater than the initial value, the value will increase.
+    
+    `decay_start : int` - The inclusive start step of the decay range.
+    
+    `decay_stop : int` - The exclusive stop step of the decay range.
+    
+    `decay_rate : float = 1.0` - The decay rate, this is the factor of the decay range to which the parameter approaches its final value.
+    For example, a decay rate of 0.5 will result in a line that approaches the final value half way between the decay start and decay end bound.
+    
+    Returns
+    -------
+    `(int) -> float` - A sinusoidal parameter decay function, returns the parameter value at a given iteration.
+    The arguments given to this functions are assigned as attributes to the function instance returned.
+    
+    Raises
+    ------
+    `ValueError` - If the decay rate is less than 0.0.
+    
+    `ValueError` - If the decay start is greater than the decay end.
     """
     if decay_rate < 0.0:
-        raise ValueError(f"Decay constant must be greater than 0.0. Got; {decay_rate}.")
+        raise ValueError(f"Decay rate must be greater than 0.0. Got; {decay_rate}.")
+    if decay_start > decay_end:
+        raise ValueError(f"Decay start must be less than decay end. Got; {decay_start} > {decay_end}.")
     
     ## Decay is exclusive of the decay end iteration.
     decay_end -= 1
@@ -202,12 +291,19 @@ def sinusoidal_decay_function(initial_value: float,
     decay_range_rated: float = decay_range * decay_rate
     
     def sinusoidal_decay(iteration: int) -> float:
+        """Sinusoidal decay function."""
         decay_iterations: int = min(decay_range, max(0, iteration - decay_start))
         if decay_iterations == 0:
             return initial_value
         elif decay_iterations < decay_range_rated:
             return final_value + (value_range * ((math.cos(math.pi * (decay_iterations / decay_range_rated)) / 2.0) + 0.5))
         return final_value
+    
+    ## Assign arguments as attributes to the function.
+    _locals = locals()
+    for param in linear_decay_function.__annotations__:
+        if param != "return":
+            setattr(sinusoidal_decay, param, _locals[param])
     
     return sinusoidal_decay
 
@@ -223,31 +319,37 @@ def get_decay_function(decay_type: DecayFunctionType,
     
     Parameters
     ----------
-    `decay_type : str` - The type of decay function to create:
+    `decay_type : str | None` - The type of decay function to create:
         - "lin" - Linear decay function.
         - "pol" - Polynomial decay function.
         - "exp" - Exponential decay function.
-        - "b-exp" - Bounded exponential decay function.
         - "sin" - Sinusoidal decay function.
+    If None, then a indentity function is created, which always returns the initial value.
     
     `initial_value : float` - The initial value of the parameter.
     
-    `decay_constant : float` - The decay constant.
+    `final_value : float` - The final value of the parameter.
+    If the final value is less than the initial value, the parameter value will decrease over the decay range.
+    If the final value is greater than the initial value, the value will increase.
     
-    `decay_start : int` - The iteration at which decay starts.
+    `decay_start : int` - The inclusive start step of the decay range.
     
-    `decay_end : int` - The iteration at which decay ends.
+    `decay_stop : int` - The exclusive stop step of the decay range.
     
-    `iterations_limit : int` - The maximum number of iterations.
+    `decay_rate : float = 1.0` - The decay rate, this is the factor of the decay range to which the parameter approaches its final value.
+    For example, a decay rate of 0.5 will result in a line that approaches the final value half way between the decay start and decay end bound.
     
     Returns
     -------
-    `(int) -> float` - The decay function.
-    Takes the iteration number as argument and returns the parameter value for that iteration.
+    `(int) -> float` - The decay function, returns the parameter value at a given iteration.
     
     Raises
     ------
     `ValueError` - If the decay type is not one of the supported types.
+    
+    `ValueError` - If the decay rate is less than 0.0.
+    
+    `ValueError` - If the decay start is greater than the decay end.
     """
     match decay_type:
         case "lin":
@@ -259,3 +361,15 @@ def get_decay_function(decay_type: DecayFunctionType,
         case "sin":
             return sinusoidal_decay_function(initial_value, final_value, decay_start, decay_end, decay_rate)
         case _: ValueError(f"Unknown decay type: {decay_type}")
+    
+    def identity_decay(iteration: int) -> float:
+        """Identity decay function."""
+        return initial_value
+    
+    ## Assign arguments as attributes to the function.
+    _locals = locals()
+    for param in get_decay_function.__annotations__:
+        if param not in ["decay_type", "return"]:
+            setattr(identity_decay, param, _locals[param])
+    
+    return identity_decay
