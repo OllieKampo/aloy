6c0cc87d5af5d2a5d5daf51522c3158c14ab3100
Oliver Michael Kamperis <olliekampo@gmail.com>
2023-03-26 00:28:14 +0000

    More work on snake game.

--- guis/games/snakegame.py
+++ guis/games/snakegame.py
@@ -1,8 +1,12 @@
 
-from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QPushButton, QSizePolicy, QSpacerItem, QSpacerItem
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QPushButton, QSizePolicy, QSpacerItem, QSpacerItem, QGraphicsScene, QGraphicsView, QMainWindow, QGraphicsItem
+from PyQt6.QtCore import Qt, QTimer, QEvent, QPoint, QRect, QSize
+from PyQt6.QtGui import QPainter, QBrush, QPen, QColor, QPalette, QKeySequence
 from guis.gui import JinxGuiData
 
 from guis.observable import Observable, Observer
+
+import random
 
 class SnakeGame(Observer):
     """
@@ -10,34 +14,177 @@ class SnakeGame(Observer):
     """
 
     def __init__(self, parent: QWidget) -> None:
-        self.__gui_data: JinxGuiData = JinxGuiData()
+        super().__init__()
+        self.__gui_data = JinxGuiData()
         self.__gui_data.assign_observers(self)
 
-        self.__snake = None
-        self.__food = None
+        self.__score: int = 0
+        self.__direction: tuple[int, int] = (1, 0)
+        self.__speed: int = 1
+        self.__seconds_per_move: float = 0.5
+        self.__game_over: bool = False
+        self.__paused: bool = False
+        self.__grid_size: tuple[int, int] = (20, 20)
+        self.__snake: list[tuple[int, int]] = []
+        self.__food: tuple[int, int] | None = None
 
+        ## Set up the parent widget and layout
         self.__parent: QWidget = parent
-        self.__layout: QGridLayout = QGridLayout()
+        self.__layout = QGridLayout()
         self.__layout.setContentsMargins(0, 0, 0, 0)
         self.__layout.setSpacing(0)
         self.__parent.setLayout(self.__layout)
-        self.__parent.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.__parent.setSizePolicy(
+            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+        )
         self.__parent.setStyleSheet("background-color: black;")
+        self.__parent.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
 
+        ## Score label
         self.__score_label: QLabel = QLabel("Score: 0")
         self.__score_label.setStyleSheet("color: white;")
         self.__layout.addWidget(self.__score_label, 0, 0)
 
+        ## Restart button (reset score, snake, and food)
         self.__restart_button: QPushButton = QPushButton("Restart")
         self.__restart_button.setStyleSheet("color: white;")
         self.__restart_button.clicked.connect(self.__restart)
         self.__layout.addWidget(self.__restart_button, 0, 1)
 
-        self.__spacer: QSpacerItem = QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        ## Add are scene to draw the snake and food on
+        self.__scene = QGraphicsScene()
+        self.__view = QGraphicsView(self.__scene)
+        self.__view.setStyleSheet("background-color: white;")
+        self.__view.setSizePolicy(
+            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+        )
+        self.__layout.addWidget(self.__view, 1, 0, 1, 2)
+        self.__restart()
+
+        ## Draw the snake and food
+        self.__draw_snake_and_food()
+        
+        ## Add a spacer to the bottom of the layout
+        self.__spacer = QSpacerItem(
+            0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+        )
         self.__layout.addItem(self.__spacer, 1, 0, 1, 2)
+
+        ## Set up the timer to move the snake
+        self.__timer = QTimer()
+        self.__timer.setInterval(1000)
+        self.__timer.timeout.connect(self.__move_snake)
+        self.__timer.start()
+
+        ## Set up the key press event
+        self.__parent.keyPressEvent = self.__key_press_event
+    
+    def __key_press_event(self, event: QEvent) -> None:
+        """Handle key press events."""
+        print("Key Press: ", event.key())
+        if event.key() == Qt.Key.Key_Up:
+            self.__direction = (0, -1)
+        elif event.key() == Qt.Key.Key_Down:
+            self.__direction = (0, 1)
+        elif event.key() == Qt.Key.Key_Left:
+            self.__direction = (-1, 0)
+        elif event.key() == Qt.Key.Key_Right:
+            self.__direction = (1, 0)
+        elif event.key() == Qt.Key.Key_Space:
+            self.__paused = not self.__paused
+
+    def __move_snake(self) -> None:
+        """Move the snake in the current direction."""
+        if self.__game_over or self.__paused:
+            return
+
+        ## Get the current head of the snake
+        x, y = self.__snake[0]
+
+        ## Get the new head of the snake
+        new_x = (x + self.__direction[0]) % self.__grid_size[0]
+        new_y = (y + self.__direction[1]) % self.__grid_size[1]
+        new_head = (new_x, new_y)
+
+        ## Check if the snake has hit itself
+        if new_head in self.__snake:
+            self.__game_over = True
+            return
+
+        ## Check if the snake has eaten the food
+        if new_head == self.__food:
+            self.__score += 1
+            self.__update_score(self.__score)
+            self.__random_food()
+        else:
+            self.__snake.pop()
+
+        ## Add the new head to the snake
+        self.__snake.insert(0, new_head)
+
+        ## Draw the snake and food
+        self.__draw_snake_and_food()
+        self.__parent.update()
+
+    def __draw_snake_and_food(self) -> None:
+        """Draw the snake and food on the grid."""
+        self.__scene.clear()
+        self.__draw_snake()
+        self.__draw_food()
+
+    def __draw_snake(self) -> None:
+        """Draw the snake on the grid."""
+        for x, y in self.__snake:
+            self.__scene.addRect(
+                x * 20,
+                y * 20,
+                20,
+                20,
+                QPen(QColor("black")),
+                QBrush(QColor("green")),
+            )
+    
+    def __draw_food(self) -> None:
+        """Draw the food on the grid."""
+        if self.__food is not None:
+            x, y = self.__food
+            self.__scene.addRect(
+                x * 20,
+                y * 20,
+                20,
+                20,
+                QPen(QColor("black")),
+                QBrush(QColor("red")),
+            )
+
+    def __random_start(self) -> None:
+        """Start the snake at a random location."""
+        self.__snake = [
+            (random.randint(0, self.__grid_size[0] - 1),
+             random.randint(0, self.__grid_size[1] - 1))
+        ]
+    
+    def __random_food(self) -> None:
+        """Place food at a random location."""
+        while True:
+            food = (
+                random.randint(0, self.__grid_size[0] - 1),
+                random.randint(0, self.__grid_size[1] - 1),
+            )
+            if food not in self.__snake:
+                self.__food = food
+                break
     
     def __restart(self) -> None:
-        pass
+        """Reset the game."""
+        self.__score = 0
+        self.__update_score(self.__score)
+        self.__random_start()
+        self.__random_food()
+        self.__game_over = False
+        self.__paused = False
+        self.__draw_snake_and_food()
+        self.__parent.update()
 
     def __update_score(self, score: int) -> None:
         self.__score_label.setText(f"Score: {score}")
@@ -45,10 +192,12 @@ class SnakeGame(Observer):
 if __name__ == "__main__":
     # You need one (and only one) QApplication instance per application.
     app = QApplication([])
-    window = QWidget()
+    window = QMainWindow()
     window.setWindowTitle("Snake Game")
     window.resize(800, 600)
-    snake_game = SnakeGame(window)
+    widget = QWidget()
+    window.setCentralWidget(widget)
+    snake_game = SnakeGame(widget)
     window.show()
 
     app.exec()
--- guis/gui.py
+++ guis/gui.py
@@ -6,7 +6,10 @@ import guis.observable as observable
 
 class JinxGuiData(observable.Observable):
 
-    __slots__ = ("__desired_view_state", "__view_states")
+    __slots__ = (
+        "__desired_view_state",
+        "__view_states"
+    )
     
     def __init__(self) -> None:
         super().__init__()
@@ -32,83 +35,82 @@ class JinxGuiData(observable.Observable):
         """Set the current desired view state name."""
         self.__desired_view_state = desired_view_state
 
-class ObserverWidget(QtWidgets.QWidget, observable.Observer):
-    """A class defining an observer PyQt6 widget."""
+class JinxObserverWidget(observable.Observer):
+    """A class defining an observer widget."""
 
     def __init__(self,
                  parent: QtWidgets.QWidget | None = None,
                  flags: QtCore.Qt.WindowType = QtCore.Qt.WindowType.Widget
                  ) -> None:
         """Create a new Jinx widget within the given parent widget."""
-        super().__init__(parent, flags)
-        observable.Observer.__init__(self)
+        super().__init__(self)
     
     def update_observer(self, data: JinxGuiData) -> None:
         pass
 
-def make_observer_widget(parent: QtWidgets.QWidget | None = None,
-                         flags: QtCore.Qt.WindowType = QtCore.Qt.WindowType.Widget,
-                         update_observer: Callable[[JinxGuiData], None] = lambda data: None
-                         ) -> ObserverWidget:
-    """Create a new observer widget."""
-    return type("AnonymousObserverWidget", (ObserverWidget,),
-                {"update_observer": update_observer})(parent, flags)
+# def make_observer_widget(parent: QtWidgets.QWidget | None = None,
+#                          flags: QtCore.Qt.WindowType = QtCore.Qt.WindowType.Widget,
+#                          update_observer: Callable[[JinxGuiData], None] = lambda data: None
+#                          ) -> ObserverWidget:
+#     """Create a new observer widget."""
+#     return type("AnonymousObserverWidget", (ObserverWidget,),
+#                 {"update_observer": update_observer})(parent, flags)
 
-class JinxGuiWindow(observable.Observer):
-    """A class defining a PyQt6 window used by Jinx."""
+# class JinxGuiWindow(observable.Observer):
+#     """A class defining a PyQt6 window used by Jinx."""
 
-    __slots__ = ("__data",
-                 "__view_states",
-                 "__current_view_state",
-                 "__window")
+#     __slots__ = ("__data",
+#                  "__view_states",
+#                  "__current_view_state",
+#                  "__window")
     
-    def __init__(self, parent: QtWidgets.QWidget, data: JinxGuiData) -> None:
-        """Create a new Jinx window within the given parent widget."""
-        observable.Observer.__init__(self)
+#     def __init__(self, parent: QtWidgets.QMainWindow, data: JinxGuiData) -> None:
+#         """Create a new Jinx window within the given parent widget."""
+#         observable.Observer.__init__(self)
 
-        self.__parent: QtWidgets.QWidget = parent
-        vbox = QtWidgets.QVBoxLayout()
-        self.__stack = QtWidgets.QStackedWidget()
-        self.__combo_box = QtWidgets.QComboBox()
-        self.__combo_box.currentTextChanged.connect(self.__combo_box_changed)
-        vbox.addWidget(self.__combo_box)
-        vbox.addWidget(self.__stack)
-        self.__parent.setLayout(vbox)
+#         self.__parent: QtWidgets.QWidget = parent
+#         vbox = QtWidgets.QVBoxLayout()
+#         self.__stack = QtWidgets.QStackedWidget()
+#         self.__combo_box = QtWidgets.QComboBox()
+#         self.__combo_box.currentTextChanged.connect(self.__combo_box_changed)
+#         vbox.addWidget(self.__combo_box)
+#         vbox.addWidget(self.__stack)
+#         self.__parent.setLayout(vbox)
 
-        self.__data: JinxGuiData = data
-        self.__data.assign_observers(self)
+#         self.__data: JinxGuiData = data
+#         self.__data.assign_observers(self)
         
-        self.__view_states: dict[str, ObserverWidget] = {}
-        self.__current_view_state: str | None = None
-        self.__data.desired_view_state = None
+#         self.__view_states: dict[str, ObserverWidget] = {}
+#         self.__current_view_state: str | None = None
+#         self.__data.desired_view_state = None
 
-        self.__data.notify_all()
+#         self.__data.notify_all()
     
-    @property
-    def current_view_state(self) -> str:
-        """Get the current view state of the window."""
-        return self.__current_view_state
+#     @property
+#     def current_view_state(self) -> str:
+#         """Get the current view state of the window."""
+#         return self.__current_view_state
     
-    def add_view_state(self, view_state: str, widget: ObserverWidget) -> None:
-        """Add a new view state to the window."""
-        self.__view_states[view_state] = widget
+#     def add_view_state(self, view_state: str, widget: ObserverWidget) -> None:
+#         """Add a new view state to the window."""
+#         self.__view_states[view_state] = widget
     
-    def remove_view_state(self, view_state: str) -> None:
-        """Remove a view state from the window."""
-        self.__view_states.pop(view_state)
+#     def remove_view_state(self, view_state: str) -> None:
+#         """Remove a view state from the window."""
+#         self.__view_states.pop(view_state)
     
-    def update_observer(self, data: JinxGuiData) -> None:
-        desired_view_state: str | None = self.__data.desired_view_state
-        if self.__current_view_state != desired_view_state:
-            self.__data.clear_observers()
-            self.__data.assign_observers(self)
-            if desired_view_state is not None:
-                widget: ObserverWidget = self.__view_states[desired_view_state]
-                self.__stack.setCurrentWidget(widget)
-                self.__data.assign_observers(widget)
-            self.__current_view_state = desired_view_state
-            self.__data.notify_all()
+#     def update_observer(self, data: JinxGuiData) -> None:
+#         desired_view_state: str | None = self.__data.desired_view_state
+#         if self.__current_view_state != desired_view_state:
+#             self.__data.clear_observers()
+#             self.__data.assign_observers(self)
+#             if desired_view_state is not None:
+#                 widget: ObserverWidget = self.__view_states[desired_view_state]
+#                 self.__stack.setCurrentWidget(widget)
+#                 self.__data.assign_observers(widget)
+#             self.__current_view_state = desired_view_state
+#             self.__data.notify_all()
     
-    def __combo_box_changed(self, index: int) -> None:
-        """Handle a change in the combo box."""
-        self.__data.desired_view_state = self.__combo_box.currentText()
+#     def __combo_box_changed(self, index: int) -> None:
+#         """Handle a change in the combo box."""
+#         self.__data.desired_view_state = self.__combo_box.currentText()
