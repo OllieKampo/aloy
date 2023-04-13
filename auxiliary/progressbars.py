6435dfc2e99b8b4848177ad4d5c3ce6f4e890c54
Oliver Michael Kamperis <olliekampo@gmail.com>
2023-04-13 22:39:04 +0100

    Updated ResourceProgressBar.

--- auxiliary/progressbars.py
+++ auxiliary/progressbars.py
@@ -22,10 +22,12 @@
 
 """Module defining custom CLI progress bars."""
 
+from contextlib import contextmanager
 import os
+import sys
 import threading
 from time import sleep
-from typing import Optional
+from typing import Any, Iterable, Iterator
 import psutil
 from tqdm import tqdm
 
@@ -54,20 +56,25 @@ class ResourceProgressBar:
         "__process": "Process used for getting resource usage statistics.",
         "__postfix": "Postfix dictionary used for updating the progress bar.",
         "__progress_bar": "The progress bar itself.",
+        "__resource_update_interval": "Interval between resource updates.",
         "__running": "A boolean variable used for stopping the update thread.",
         "__cpu_thread": "Thread used for updating resource statistics."
     }
 
-    def __init__(self,
-                 initial: int = 0,
-                 total: Optional[int] = None,
-                 desc: Optional[str] = None,
-                 unit: str = "it",
-                 leave: bool = False,
-                 ncols: int = 180,
-                 miniters: int = 1,
-                 colour: str = "cyan"
-                 ) -> None:
+    def __init__(
+        self,
+        iterable: Iterable[Any] | None = None,
+        initial: int | float = 0,
+        total: int | float | None = None,
+        desc: str | None = None,
+        unit: str = "it",
+        leave: bool = False,
+        ncols: int | None = None,
+        dynamic_ncols: bool = False,
+        miniters: int = 1,
+        colour: str = "cyan",
+        resource_update_interval: float = 0.1
+    ) -> None:
         """
         Create a resouce usage progress bar.
 
@@ -82,19 +89,22 @@ class ResourceProgressBar:
             "CPU(%)": self.__get_cpu()
         }
         self.__progress_bar = tqdm(
-            postfix=self.__postfix,
+            iterable=iterable,
             initial=initial,
             total=total,
             desc=desc,
             unit=unit,
             leave=leave,
             ncols=ncols,
+            dynamic_ncols=dynamic_ncols,
             miniters=miniters,
-            colour=colour
+            colour=colour,
+            postfix=self.__postfix
         )
 
         # Variables for running an additional thread which
         # updates resource statistics ten times per second.
+        self.__resource_update_interval: float = resource_update_interval
         self.__running: bool = True
         self.__cpu_thread = threading.Thread(target=self.__update)
         self.__cpu_thread.daemon = True
@@ -114,12 +124,34 @@ class ResourceProgressBar:
         while self.__running:
             self.__postfix["Mem(Mb)"] = self.__get_mem()
             self.__postfix["CPU(%)"] = self.__get_cpu()
-            sleep(0.1)
+            sleep(self.__resource_update_interval)
 
-    def update(self,
-               n: int = 1,
-               data: Optional[dict[str, str]] = None
-               ) -> None:
+    def __iter__(self) -> Iterator[Any]:
+        """
+        Iterate over the progress bar.
+
+        Raises an exception if an iterable was not provided to the upon
+        creation of the progress bar.
+        """
+        for val in self.__progress_bar:
+            self.__progress_bar.set_postfix(self.__postfix)
+            yield val
+
+    def get_resource_usage(self) -> tuple[float, float]:
+        """
+        Get the current memory and CPU usage.
+
+        Returns a tuple of the form `(memory: float, cpu: float)`, where
+        memory is the current memory usage in megabytes and cpu is the current
+        CPU usage in percent.
+        """
+        return (float(self.__get_mem()), float(self.__get_cpu()))
+
+    def update(
+        self,
+        n: int = 1, /,
+        data: dict[str, str] | None = None
+    ) -> None:
         """
         Update the progress bar.
 
@@ -132,16 +164,102 @@ class ResourceProgressBar:
         given as a mapping of name to value pairs. An empty dictionary will
         clear the postfix, None will leave the postfix unchanged.
         """
-        self.__progress_bar.set_postfix(data | self.__postfix
-                                        if data is not None
-                                        else self.__postfix)
+        if data is not None:
+            self.__progress_bar.set_postfix(data | self.__postfix)
+        else:
+            self.__progress_bar.set_postfix(self.__postfix)
         self.__progress_bar.update(n)
+
+    def set_description(self, desc: str) -> None:
+        """Set the progress bar's description (its prefix)."""
+        self.__progress_bar.set_description(desc)
+
+    def write(self, msg: str, /) -> None:
+        """
+        Write a single message to the console without overlapping the progress
+        bar.
+
+        Message will be written above the progress bar.
+
+        This is essentially equal to:
+        ```
+        bar.clear()
+        print("This is a test.")
+        bar.refresh()
+        ```
+        or
+        ```
+        with bar.external_write_mode():
+            print("This is a test.")
+        ```
+        """
+        self.__progress_bar.write(msg)
+
+    @contextmanager
+    def external_write_mode(self) -> Iterator[None]:
+        """
+        Set the progress bar to external write mode.
+
+        This clears the progress bar and allows printing to the console
+        without overlapping the progress bar. When the context manager
+        exits, the progress bar is re-displayed, below any printed messages.
+        """
+        try:
+            with self.__progress_bar.external_write_mode():
+                yield None
+        finally:
+            pass
+
+    def get_input(self, prompt: str, /) -> str:
+        """
+        Clear the progress bar, display a prompt, and pause for user input.
+
+        When user input is received, the prompt is cleared and the progress
+        bar is re-displayed.
+        """
+        self.__progress_bar.clear()
+        user_input = input(prompt)
+        sys.stdout.write("\033[A")
+        sys.stdout.flush()
+        self.__progress_bar.refresh()
+        return user_input
+
+    def reset(self, total: int | float | None = None) -> None:
+        """
+        Reset the progress bar to 0 iterations for repeated use
+        (generally more efficient than creating a new instance).
+
+        Consider combining with `leave=True`.
+
+        If `total` is specified, then use it as the total for the new
+        progress bar.
+        """
+        self.__progress_bar.reset(total)
+
+    def clear(self) -> None:
+        """
+        Clear the progress bar.
+
+        This will remove the progress bar from the terminal.
+        """
+        self.__progress_bar.clear()
+
+    def refresh(self) -> None:
+        """
+        Refresh the progress bar.
+
+        This will update the progress bar's display. If the progress bar
+        was previously cleared, this will re-display it.
+        """
+        self.__progress_bar.refresh()
 
     def close(self, wait: bool = False) -> None:
         """
         Cleanup and close the progress bar.
 
         If `wait` is True, block until the progress bar is closed.
+
+        Once closed, the progress bar cannot be re-opened.
         """
         self.__progress_bar.close()
         self.__running = False
