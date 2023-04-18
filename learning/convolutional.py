64980c15ba89ff0ef4d07cf84fe699da56bed227
Oliver Michael Kamperis <olliekampo@gmail.com>
2023-04-18 23:13:35 +0100

    Add some more convolutional helpers.

--- learning/convolutional.py
+++ learning/convolutional.py
@@ -4,10 +4,12 @@ Module containing utility functions for convolutional neural network layers.
 """
 
 
+from typing import TypeAlias
+import torch
 import torch.nn as nn
 
 
-def calc_output_len(
+def calc_conv_output_len(
     input_size: int,
     kernel_size: int,
     stride: int,
@@ -15,10 +17,21 @@ def calc_output_len(
     dilation: int
 ) -> int:
     """
-    Calculate the output length over one dimension of a convolutional layer.
+    Calculate the output length over one dimension of a convolutional layer
+    1d, 2d, or 3d.
 
-    See: https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
+    Formula is:
+    ```
+    len_ = (((input_size
+              + (2 * padding)
+              - (dilation * (kernel_size - 1))
+              - 1)
+             // stride)
+            + 1)
+    ```
+
     Stride: https://deepai.org/machine-learning-glossary-and-terms/stride
+
     Padding: https://deepai.org/machine-learning-glossary-and-terms/padding
     """
     return (((input_size
@@ -29,7 +42,39 @@ def calc_output_len(
             + 1)
 
 
-def calc_output_shape(
+def calc_conv_transpose_output_len(
+    input_size: int,
+    kernel_size: int,
+    stride: int,
+    padding: int,
+    dilation: int,
+    output_padding: int
+) -> int:
+    """
+    Calculate the output length over one dimension of a convolutional
+    transpose layer 1d, 2d, or 3d.
+
+    Formula is:
+    ```
+    len_ = (stride * (input_size - 1)
+            + (dilation * (kernel_size - 1))
+            - (2 * padding)
+            + output_padding
+            + 1)
+    ```
+
+    Stride: https://deepai.org/machine-learning-glossary-and-terms/stride
+
+    Padding: https://deepai.org/machine-learning-glossary-and-terms/padding
+    """
+    return (stride * (input_size - 1)
+            + (dilation * (kernel_size - 1))
+            - (2 * padding)
+            + output_padding
+            + 1)
+
+
+def calc_conv_output_shape(
     input_size: tuple[int, ...],
     kernel_size: tuple[int, ...] | int,
     stride: tuple[int, ...] | int,
@@ -38,8 +83,6 @@ def calc_output_shape(
 ) -> tuple[int, ...]:
     """
     Calculate the output shape of a 2d or 3d convolutional layer (per channel).
-
-    See: https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
     """
     dimensions = len(input_size)
     if isinstance(kernel_size, int):
@@ -51,22 +94,63 @@ def calc_output_shape(
     if isinstance(dilation, int):
         dilation = (dilation,) * dimensions
     return tuple(
-        calc_output_len(input_, kernel_, stride_, padding_, dilation_)
+        calc_conv_output_len(input_, kernel_, stride_, padding_, dilation_)
         for input_, kernel_, stride_, padding_, dilation_
         in zip(input_size, kernel_size, stride, padding, dilation)
     )
 
 
-def calc_output_shape_from(
+def calc_conv_transpose_output_shape(
     input_size: tuple[int, ...],
-    layer: nn.Conv2d | nn.Conv3d
+    kernel_size: tuple[int, ...] | int,
+    stride: tuple[int, ...] | int,
+    padding: tuple[int, ...] | int,
+    dilation: tuple[int, ...] | int,
+    output_padding: tuple[int, ...] | int
 ) -> tuple[int, ...]:
     """
-    Calculate the output shape of a 2d or 3d convolutional layer (per channel).
-
-    See: https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
+    Calculate the output shape of a 2d or 3d convolutional transpose layer (per channel).
     """
-    return calc_output_shape(
+    dimensions = len(input_size)
+    if isinstance(kernel_size, int):
+        kernel_size = (kernel_size,) * dimensions
+    if isinstance(stride, int):
+        stride = (stride,) * dimensions
+    if isinstance(padding, int):
+        padding = (padding,) * dimensions
+    if isinstance(dilation, int):
+        dilation = (dilation,) * dimensions
+    if isinstance(output_padding, int):
+        output_padding = (output_padding,) * dimensions
+    return tuple(
+        calc_conv_transpose_output_len(
+            input_, kernel_, stride_, padding_, dilation_, output_padding_
+        )
+        for input_, kernel_, stride_, padding_, dilation_, output_padding_
+        in zip(input_size, kernel_size, stride, padding, dilation,
+               output_padding)
+    )
+
+
+ConvolutionalLayer: TypeAlias = (
+    nn.Conv1d |
+    nn.Conv2d |
+    nn.Conv3d |
+    nn.ConvTranspose1d |
+    nn.ConvTranspose2d |
+    nn.ConvTranspose3d
+)
+
+
+def calc_conv_output_shape_from(
+    input_size: tuple[int, ...],
+    layer: ConvolutionalLayer
+) -> tuple[int, ...]:
+    """
+    Calculate the output shape of a 1d, 2d, or 3d standard or transpose
+    convolutional layer (per channel).
+    """
+    return calc_conv_output_shape(
         input_size=input_size,
         kernel_size=layer.kernel_size,
         stride=layer.stride,
@@ -85,3 +169,23 @@ def size_of_flat_layer(
     See: https://pytorch.org/docs/stable/generated/torch.nn.Flatten.html
     """
     return output_shape[0] * output_shape[1] * num_channels
+
+
+def pad_circular(
+    input_: torch.Tensor,
+    padding: tuple[int, ...] | int
+) -> torch.Tensor:
+    """
+    Pad a tensor with circular padding.
+
+    Essentially, this copies the top `padding` rows and adds them to the
+    bottom, the bottom and adds them to the top, the left columns and adds
+    them to the right, and the right column and adds it to the left.
+    """
+    if isinstance(padding, int):
+        padding = (padding,) * 4
+    return torch.nn.functional.pad(
+        input=input_,
+        pad=padding,
+        mode="circular"
+    )
