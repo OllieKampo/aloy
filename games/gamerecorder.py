94a88f845174c98498c450754772252f6edc03b5
Oliver Kamperis <olliekampo@gmail.com>
2023-06-19 22:39:54 +0100

    Add support for matches in game recorder.

--- games/gamerecorder.py
+++ games/gamerecorder.py
@@ -29,34 +29,66 @@ ST = TypeVar("ST")
 class GameRecorder(Generic[AT, ST]):  # pylint: disable=too-few-public-methods
     """Class recording actions played in a game, and saving them to a file."""
 
-    def __init__(self, game_spec: GameSpec, initial_state: ST) -> None:
+    def __init__(self, game_spec: GameSpec, initial_state: ST | None = None) -> None:
         """Initialize a GameRecorder."""
-        self.game_spec = game_spec
-        self.initial_state = initial_state
-        self.records: list[dict[str, AT | ST | None]] = []
+        self.__game_spec: list[GameSpec] = [game_spec]
+        self.__initial_state: list[ST] = [initial_state]
+        self.__records: list[list[dict[str, AT | ST | None]]] = [[]]
+        self.__last_game_spec: GameSpec = game_spec
+        self.__last_initial_state: ST = initial_state
+
+    def new_match(
+        self,
+        game_spec: GameSpec | None = None,
+        initial_state: ST | None = None
+    ) -> None:
+        """Start recording a new match."""
+        if game_spec is None:
+            if self.__game_spec:
+                game_spec = self.__game_spec[-1]
+            else:
+                game_spec = self.__last_game_spec
+        self.__last_game_spec = game_spec
+        self.__game_spec.append(game_spec)
+        if initial_state is None:
+            if self.__initial_state:
+                initial_state = self.__initial_state[-1]
+            else:
+                initial_state = self.__last_initial_state
+        self.__last_initial_state = initial_state
+        self.__initial_state.append(initial_state)
+        self.__records.append([])
+
+    def clear_last_match(self) -> None:
+        """Clear the last match."""
+        if self.__game_spec:
+            self.__game_spec.pop()
+            self.__initial_state.pop()
+            self.__records.pop()
 
     def record(self, action: AT, state: ST | None = None) -> None:
         """Record an action."""
-        self.records.append({"action": action, "state": state})
+        self.__records[-1].append({"action": action, "state": state})
 
-    def save(self, filename: str, id: int | None = None, append: bool = True) -> None:
+    def save(self, filename: str, append: bool = True) -> None:
         """Save the recorded actions to a file."""
-        if id is None:
-            id = random.randint(0, 2 ** 64 - 1)
-        data = [{
-            "game": self.game_spec.game_name,
-            "match": self.game_spec.match_name,
-            "id": id,
-            "date": datetime.datetime.now().isoformat(),
-            "options": self.game_spec.game_options,
-            "initial_state": self.initial_state,
-            "records": self.records
-        }]
+        data = [
+            {
+                "game": self.__game_spec[i].game_name,
+                "match": self.__game_spec[i].match_name,
+                "id": i,
+                "date": datetime.datetime.now().isoformat(),
+                "options": self.__game_spec[i].game_options,
+                "initial_state": self.__initial_state[i],
+                "records": self.__records[i]
+            }
+            for i in range(len(self.__game_spec))
+        ]
         # For open modes see https://stackoverflow.com/a/30566011
         # For IO see https://docs.python.org/3/library/io.html#i-o-base-classes
-        # Since UTF-8 does not have uniform character width (variable-length character encoding),
-        # we cannot use seek() reliably move a number of characters relative to the end of the file.
-        # See here for some discussion:
+        # Since UTF-8 does not have uniform character width (variable-length
+        # character encoding), we cannot use seek() reliably move a number of
+        # characters relative to the end of the file. See here for discussion:
         #   - https://stackoverflow.com/a/53310360
         #   - https://stackoverflow.com/a/18857381
         #   - https://stackoverflow.com/a/21533561
@@ -74,6 +106,9 @@ class GameRecorder(Generic[AT, ST]):  # pylint: disable=too-few-public-methods
         else:
             with open(filename, "w") as file:
                 json.dump(data, file, indent=2)
+        self.game_spec = []
+        self.initial_state = []
+        self.records = []
 
 
 def __main() -> None:
