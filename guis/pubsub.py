636b44fe0adfc9ea2fb0575975817475eadad0f0
Oliver Michael Kamperis <olliekampo@gmail.com>
2023-07-02 20:57:20 +0100

    Add notes on pubsub.

--- guis/pubsub.py
+++ guis/pubsub.py
@@ -20,9 +20,11 @@
 ###########################################################################
 ###########################################################################
 
-"""Module defining publisher-subscriber interface pattern."""
+"""
+Module defining publisher-subscriber interface pattern.
 
-from abc import ABCMeta
+An alternative to explicit naming of source and destination would be to name a port through which communication is to take place.
+"""
 
 __copyright__ = "Copyright (C) 2023 Oliver Michael Kamperis"
 __license__ = "GPL-3.0"
@@ -30,22 +32,124 @@ __license__ = "GPL-3.0"
 __all__ = ()
 
 
+from typing import Any
+
+
 def __dir__() -> tuple[str, ...]:
     """Get the names of module attributes."""
     return __all__
 
 
-class Publisher(metaclass=ABCMeta):
+# Publish
+# Transmit
+# Report
+# Broadcast
+
+
+class MessageTopic:
+    """
+    A topic is a particular topic of communication. Publishers publish to a topic
+    by sending messages to it, and subscribers subscribe to a topic to receive
+    messages from it. Publishers publish messages to topics without knowledge
+    of what (if any) subscribers there may be. Similarly, subscribers receive
+    messages from topics without knowledge of what (if any) publishers there
+    are. This decoupling of publishers and subscribers can allow for greater
+    scalability and a more dynamic network topology. Multiple publishers can
+    publish messages to the same topic, and multiple subscribers can subscribe
+    to the same topic, and is therefore a multi-producer multi-consumer model.
+    Each publisher and subscriber is decoupled from the others, and can
+    continue operating normally regardless of the status of the others.
+    """
     pass
 
 
-class Subscriber(metaclass=ABCMeta):
+class DataField:
+    """
+    A data field is a particular field of data. Publishers publish data to a
+    data field by sending messages to it, and subscribers subscribe to a data
+    field to receive data from it. Publishers publish data to data fields
+    without knowledge of what (if any) subscribers there may be. Similarly,
+    subscribers receive data from data fields without knowledge of what (if
+    any) publishers there are. This decoupling of publishers and subscribers
+    can allow for greater scalability and a more dynamic network topology.
+    Multiple publishers can publish data to the same data field, and multiple
+    subscribers can subscribe to the same data field, and is therefore a
+    multi-producer multi-consumer model. Each publisher and subscriber is
+    decoupled from the others, and can continue operating normally regardless
+    of the status of the others.
+    """
     pass
 
 
-class PusSubHub(metaclass=ABCMeta):
+class ProcedureCall:
+    """
+    A procedure call is a particular procedure that can be called. Publishers
+    publish procedure calls to a topic by sending messages to it, and
+    subscribers subscribe to a topic to receive procedure calls from it.
+    Publishers publish procedure calls to topics without knowledge of what (if
+    any) subscribers there may be. Similarly, subscribers receive procedure
+    calls from topics without knowledge of what (if any) publishers there are.
+    This decoupling of publishers and subscribers can allow for greater
+    scalability and a more dynamic network topology. Multiple publishers can
+    publish procedure calls to the same topic, and multiple subscribers can
+    subscribe to the same topic, and is therefore a multi-producer
+    multi-consumer model. Each publisher and subscriber is decoupled from the
+    others, and can continue operating normally regardless of the status of the
+    others.
+    """
     pass
 
 
-class Topic(metaclass=ABCMeta):
+class CommandChannel:
+    """
+    A channel is a virtual pipe that allows a publisher to send commands and
+    receive feedback and results to and from a subscriber, relayed via the hub.
+
+    A publisher can create and connect to a named channel on the hub. The
+    connection requires the publisher to expose a signal that emits a command,
+    and two slots, one for feedback and one for results. A subscriber can
+    connect to the same channel on the hub. The connection requires the
+    subscriber to expose a slot that receives a command, and two signals, one
+    for feedback and one for results. The hub will then connect the publisher's
+    command signal to the subscriber's command slot, the subscriber's feedback
+    signal to the publisher's feedback slot, and the subscriber's results
+    signal to the publisher's results slot. The publisher can then send
+    commands to the subscriber by emitting the command signal, and the
+    subscriber can send feedback and results to the publisher by emitting the
+    feedback and results signals, respectively. The hub will relay the signals
+    from the publisher to the subscriber, and vice versa. Therefore the
+    publisher and subscriber can communicate with each other without knowledge
+    of each other's existence.
+    """
+    pass
+
+
+class Publisher:
+    def publish_on_topic(self, topic: MessageTopic, payload: Any) -> None:
+        """
+        Publish a message to a topic.
+
+        The publisher publishes a message to a topic by sending a payload to it.
+        """
+        pass
+
+
+class Subscriber:
+    pass
+
+
+class Processer:
+    """
+    A processer is a publisher-subscriber that processes messages from the hub
+    and then re-publishes the results back to the hub.
+    """
+    pass
+
+
+class PusSubHub:
+    """
+    A publisher-subscriber hub is a central place where publishers and
+    subscribers can register themselves. It is responsible for routing messages
+    from publishers to subscribers.
+    """
     pass
