bf76d4c6d218290cbc9d4145ec4a58ae57d0c98f
Oliver Michael Kamperis <olliekampo@gmail.com>
2023-07-22 17:01:31 +0100

    Tidy up pubsub.

--- guis/pubsub.py
+++ guis/pubsub.py
@@ -41,10 +41,10 @@ __license__ = "GPL-3.0"
 __all__ = ()
 
 
-from typing import Any, Callable
+from typing import Any, Callable, Final
 
 from concurrency.executors import JinxThreadPool
-from datastructures.mappings import TwoWayMap
+from concurrency.synchronization import SynchronizedMeta
 
 
 def __dir__() -> tuple[str, ...]:
@@ -52,106 +52,12 @@ def __dir__() -> tuple[str, ...]:
     return __all__
 
 
-class MessageTopic:
-    """
-    A topic is a particular topic of communication. Publishers publish to a topic
-    by sending messages to it, and subscribers subscribe to a topic to receive
-    messages from it. Publishers publish messages to topics without knowledge
-    of what (if any) subscribers there may be. Similarly, subscribers receive
-    messages from topics without knowledge of what (if any) publishers there
-    are. This decoupling of publishers and subscribers can allow for greater
-    scalability and a more dynamic network topology. Multiple publishers can
-    publish messages to the same topic, and multiple subscribers can subscribe
-    to the same topic, and is therefore a multi-producer multi-consumer model.
-    Each publisher and subscriber is decoupled from the others, and can
-    continue operating normally regardless of the status of the others.
-    """
-    pass
+_PUBSUBHUB_THREAD_POOL_EXECUTOR_MAX_WORKERS: Final[int] = 10
+_PUBSUBHUB_THREAD_POOL_EXECUTOR_PROFILE: Final[bool] = False
+_PUBSUBHUB_THREAD_POOL_EXECUTOR_LOG: Final[bool] = False
 
 
-class DataField:
-    pass
-
-
-class ProcedureCall:
-    pass
-
-
-class CommandChannel:
-    """
-    A channel is a virtual pipe that allows a publisher to send commands and
-    receive feedback and results to and from a subscriber, relayed via the hub.
-
-    A publisher can create and connect to a named channel on the hub. The
-    connection requires the publisher to expose a signal that emits a command,
-    and two slots, one for feedback and one for results. A subscriber can
-    connect to the same channel on the hub. The connection requires the
-    subscriber to expose a slot that receives a command, and two signals, one
-    for feedback and one for results. The hub will then connect the publisher's
-    command signal to the subscriber's command slot, the subscriber's feedback
-    signal to the publisher's feedback slot, and the subscriber's results
-    signal to the publisher's results slot. The publisher can then send
-    commands to the subscriber by emitting the command signal, and the
-    subscriber can send feedback and results to the publisher by emitting the
-    feedback and results signals, respectively. The hub will relay the signals
-    from the publisher to the subscriber, and vice versa. Therefore the
-    publisher and subscriber can communicate with each other without knowledge
-    of each other's existence.
-    """
-    pass
-
-
-class Publisher:
-    def publish_on_topic(self, topic: MessageTopic, payload: Any) -> None:
-        """
-        Publish a message to a topic.
-
-        The publisher publishes a message to a topic by sending a payload to it.
-        """
-        pass
-
-
-def trigger_on_topic_message_added(topic: MessageTopic) -> Any:
-    """Decorate a method to be called when a message is added to a topic."""
-    def decorator(func: Any) -> Any:
-        hub = PubSubHub()
-        hub.subscribe(topic, func)
-        return func
-    return decorator
-
-
-def trigger_on_data_field_change(field_name: str) -> Any:
-    """Decorate a method to be called when a data field is changed."""
-    pass
-
-
-class Subscriber:
-    def topic_message_added(
-        self,
-        source: "PubSubHub",
-        topic: MessageTopic
-    ) -> None:
-        pass
-
-    def data_field_changed(
-        self,
-        source: "PubSubHub",
-        field_name: str,
-        old_value: Any,
-        new_value: Any
-    ) -> None:
-        pass
-
-
-class Processer:
-    """
-    A processer is a publisher-subscriber that processes messages from the hub
-    and then re-publishes the results back to the hub.
-    """
-    pass
-
-
-class PubSubHub:
+class PubSubHub(metaclass=SynchronizedMeta):
     """
     A publisher-subscriber hub is a central place where publishers and
     subscribers can register themselves. It is responsible for routing messages
@@ -166,12 +72,35 @@ class PubSubHub:
     def __init__(self) -> None:
         self.__subscribers_topics: dict[str, list[Callable]] = {}
         self.__topics: dict[str, list[Any]] = {}
-        self.__executor = JinxThreadPool()
+        self.__executor = JinxThreadPool(
+            pool_name="PubSubHub :: Thread Pool Executor",
+            max_workers=_PUBSUBHUB_THREAD_POOL_EXECUTOR_MAX_WORKERS,
+            thread_name_prefix="PubSubHub :: Thread Pool Executor :: Thread",
+            profile=_PUBSUBHUB_THREAD_POOL_EXECUTOR_PROFILE,
+            log=_PUBSUBHUB_THREAD_POOL_EXECUTOR_LOG
+        )
 
-    def subscribe_topic(self, topic_name: str, func: Callable) -> None:
+    def subscribe_topic(
+        self,
+        topic_name: str,
+        callback: Callable[[str, Any, Any], None]
+    ) -> None:
+        """
+        A topic is a particular topic of communication. Publishers publish to a topic
+        by sending messages to it, and subscribers subscribe to a topic to receive
+        messages from it. Publishers publish messages to topics without knowledge
+        of what (if any) subscribers there may be. Similarly, subscribers receive
+        messages from topics without knowledge of what (if any) publishers there
+        are. This decoupling of publishers and subscribers can allow for greater
+        scalability and a more dynamic network topology. Multiple publishers can
+        publish messages to the same topic, and multiple subscribers can subscribe
+        to the same topic, and is therefore a multi-producer multi-consumer model.
+        Each publisher and subscriber is decoupled from the others, and can
+        continue operating normally regardless of the status of the others.
+        """
         if topic_name not in self.__subscribers_topics:
             self.__subscribers_topics[topic_name] = []
-        self.__subscribers_topics[topic_name].append(func)
+        self.__subscribers_topics[topic_name].append(callback)
 
     def publish_topic(self, topic_name: str, messages: str) -> None:
         if topic_name not in self.__topics:
@@ -192,3 +121,81 @@ class PubSubHub:
     ) -> None:
         for func in self.__subscribers_topics[topic_name]:
             func(topic_name, existing_messages, new_messages)
+
+    def subscribe_field(
+        self,
+        parameter_name: str,
+        callback: Callable[[str, Any, Any], None]
+    ) -> None:
+        pass
+
+    def publish_field(self, parameter_name: str, value: Any) -> None:
+        pass
+
+    def register_procedure_call(
+        self,
+        procedure_name: str,
+        callback: Callable[[str, Any], None]
+    ) -> None:
+        pass
+
+    def request_procedure(
+        self,
+        procedure_name: str,
+        parameters: dict[str, Any]
+    ) -> None:
+        pass
+
+    def register_command_channel(
+        self,
+        channel_name: str,
+        parameters: dict[str, Any],
+        callback: Callable[..., None]
+    ) -> None:
+        """
+        A channel is a virtual pipe that allows a publisher to send commands and
+        receive feedback and results to and from a subscriber, relayed via the hub.
+
+        A publisher can create and connect to a named channel on the hub. The
+        connection requires the publisher to expose a signal that emits a command,
+        and two slots, one for feedback and one for results. A subscriber can
+        connect to the same channel on the hub. The connection requires the
+        subscriber to expose a slot that receives a command, and two signals, one
+        for feedback and one for results. The hub will then connect the publisher's
+        command signal to the subscriber's command slot, the subscriber's feedback
+        signal to the publisher's feedback slot, and the subscriber's results
+        signal to the publisher's results slot. The publisher can then send
+        commands to the subscriber by emitting the command signal, and the
+        subscriber can send feedback and results to the publisher by emitting the
+        feedback and results signals, respectively. The hub will relay the signals
+        from the publisher to the subscriber, and vice versa. Therefore the
+        publisher and subscriber can communicate with each other without knowledge
+        of each other's existence.
+        """
+        pass
+
+    def request_command(
+        self,
+        channel_name: str,
+        arguments: dict[str, Any],
+        response_callback: Callable[[str, Any], None],
+        feedback_callback: Callable[[str, Any], None],
+        complete_callback: Callable[[str, Any], None]
+    ) -> None:
+        pass
+
+
+_PUBSUBHUB: Final[PubSubHub] = PubSubHub()
+
+
+def trigger_on_topic_message_added(topic_name: str) -> Any:
+    """Decorate a method to be called when a message is added to a topic."""
+    def decorator(func: Any) -> Any:
+        _PUBSUBHUB.subscribe_topic(topic_name, func)
+        return func
+    return decorator
+
+
+def trigger_on_data_field_change(field_name: str) -> Any:
+    """Decorate a method to be called when a data field is changed."""
+    pass
