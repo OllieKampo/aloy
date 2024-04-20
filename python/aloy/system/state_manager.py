db1b9d816877d07fc4a1d59c2e95f0f8df4b91d4
Oliver Michael Kamperis <olliekampo@gmail.com>
2024-04-20 23:52:45 +0100

    Finished making most parts of external state manager.

--- python/aloy/system/state_manager.py
+++ python/aloy/system/state_manager.py
@@ -18,7 +18,8 @@
 import functools
 import threading
 import time
-from typing import Any, Callable, Generic, TypeVar
+import types
+from typing import Any, Callable, Generic, NamedTuple, TypeVar, overload
 from aloy.concurrency.clocks import AsyncClockThread, AsyncCallStats
 
 __copyright__ = "Copyright (C) 2024 Oliver Michael Kamperis"
@@ -67,6 +68,9 @@ class ExternalState(Generic[VT]):
 RT = TypeVar("RT")
 
 
+_WRAPPER_ASSIGNMENTS = ("__module__", "__name__", "__qualname__", "__doc__")
+
+
 def declare_state(
     name: str,
     interval: float
@@ -74,28 +78,83 @@ def declare_state(
     [Callable[["ExternalStateManager"], RT]],
     Callable[["ExternalStateManager", float, float | None], RT]
 ]:
+    """
+    Decorator to declare a method of an external state manager as an external
+    state getter.
+
+    The getter must take no arguments and return the external state.
+    It is converted into a method that takes a threshold and a timeout
+    argument and returns the external state.
+
+    Parameters
+    ----------
+    `name: str` - The name of the external state.
+
+    `interval: float` - The interval at which the external state is updated
+    in seconds.
+    """
     def decorator(
         getter: Callable[["ExternalStateManager"], RT]
     ) -> Callable[["ExternalStateManager", float, float | None], RT]:
-        @functools.wraps(getter)
+        """Decorator converts the getter method."""
+
         def wrapper(
             manager: "ExternalStateManager",
             threshold: float = 0.0,
             timeout: float | None = None
         ) -> RT:
+            """Wrapper simply gets the state from the manager."""
             return manager.get_state(
                 name=name,
                 timethreshold=threshold,
                 timeout=timeout
             )
-        wrapper.__state_name__ = name
-        wrapper.__state_interval__ = interval
+
+        wrapper.__state_name__ = name  # type: ignore[attr-defined]
+        wrapper.__state_interval__ = interval  # type: ignore[attr-defined]
+        wrapper.__state_getter__ = getter  # type: ignore[attr-defined]
+
+        # Copy the module, names, and docstring of the wrapped function
+        # to the wrapper.
+        for item_name in _WRAPPER_ASSIGNMENTS:
+            item = getattr(getter, item_name, None)
+            if item is not None:
+                setattr(wrapper, item_name, item)
+
+        # Merge the dictionaries of the getter and the wrapper.
+        getattr(wrapper, "__dict__", {}).update(
+            getattr(getter, "__dict__", {}))
+
+        # Update the annotations to add the threshold and timeout
+        # parameters.
+        __annotations__ = getattr(getter, "__annotations__", {})
+        __annotations__["threshold"] = float
+        __annotations__["timeout"] = float | None
+        wrapper.__annotations__ = __annotations__
+
         return wrapper
     return decorator
 
 
-class ExternalStateMeta(type):
-    """Metaclass for external state classes."""
+class _StateGetter(NamedTuple):
+    """Named tuple to store the interval and getter of an external state."""
+
+    interval: float
+    getter: Callable[[], Any]
+
+
+def _make_state_getter(
+    getter: Callable[["ExternalStateManager"], Any]
+) -> _StateGetter:
+    """Make a state getter from a method."""
+    return _StateGetter(
+        interval=getter.__state_interval__,  # type: ignore[attr-defined]
+        getter=getter.__state_getter__  # type: ignore[attr-defined]
+    )
+
+
+class ExternalStateManagerMeta(type):
+    """Metaclass for external state manager classes."""
 
     def __new__(
         mcs,
@@ -105,15 +164,19 @@ class ExternalStateMeta(type):
     ) -> type:
         """Create a new external state class."""
         cls = super().__new__(mcs, name, bases, namespace)
-        for attr_name in dir(cls):
-            attr = getattr(cls, attr_name)
-            if callable(attr):
+        for attr_name, attr in namespace.items():
+            if (attr_name.startswith("__")
+                    or attr_name.endswith("__")):
+                continue
+            if isinstance(attr, types.FunctionType):
                 if hasattr(attr, "__state_name__"):
-                    cls.__states__[attr.__state_name__] = attr.__state_interval__
+                    cls.__states__[  # type: ignore[attr-defined]
+                        attr.__state_name__  # type: ignore[attr-defined]
+                    ] = _make_state_getter(attr)
         return cls
 
 
-class ExternalStateManager(metaclass=ExternalStateMeta):
+class ExternalStateManager(metaclass=ExternalStateManagerMeta):
     """Class to manage regular polling and storage of external state."""
 
     __slots__ = {
@@ -122,6 +185,8 @@ class ExternalStateManager(metaclass=ExternalStateMeta):
         "__conditions": "The conditions for notifying state changes.",
         "__callbacks": "The callbacks for state changes."
     }
+
+    __states__: dict[str, _StateGetter] = {}
 
     def __init__(self, max_workers: int | None = None) -> None:
         """Initialize the external state manager."""
@@ -132,7 +197,46 @@ class ExternalStateManager(metaclass=ExternalStateMeta):
             str, list[Callable[[str, ExternalState], None]]
         ] = {}
 
+        for state_name, state_getter in self.__states__.items():
+            self.add_state(
+                name=state_name,
+                interval=state_getter.interval,
+                getter=functools.partial(state_getter.getter, self)
+            )
+
+    @overload
     def add_state(
+        self,
+        name: str,
+        interval: float
+    ) -> Callable[
+        [Callable[[], Any]],
+        None
+    ]:
+        ...
+
+    @overload
+    def add_state(
+        self,
+        name: str,
+        interval: float,
+        getter: Callable[[], Any]
+    ) -> None:
+        ...
+
+    def add_state(
+        self,
+        name: str,
+        interval: float,
+        getter: Callable[[], Any] | None = None
+    ) -> Callable[[Callable[[], Any]], None] | None:
+        """Declare a setter for external state."""
+        if getter is None:
+            return self.__add_state_decorator(name, interval)
+        self.__add_state(name, interval, getter)
+        return None
+
+    def __add_state(
         self,
         name: str,
         interval: float,
@@ -146,7 +250,21 @@ class ExternalStateManager(metaclass=ExternalStateMeta):
         self.__conditions[name] = threading.Condition()
         getter_ = self.__make_getter(getter)
         setter_ = self.__make_setter(name)
-        self.__clock.schedule(interval, getter_, return_callback=setter_)
+        self.__clock.schedule(
+            interval,
+            getter_,  # type: ignore[arg-type]
+            return_callback=setter_
+        )
+
+    def __add_state_decorator(
+        self,
+        name: str,
+        interval: float
+    ) -> Callable[[Callable[[], Any]], None]:
+        """Declare a setter for external state."""
+        def add_state_decorator(getter: Callable[[], Any]) -> None:
+            self.__add_state(name, interval, getter)
+        return add_state_decorator
 
     def __make_getter(
         self,
